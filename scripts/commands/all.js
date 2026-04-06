1e76efff0d77d145dd93edea04072696dc15435b
JAHIDULLX6 <bid6074@gmail.com>
2026-04-06 20:44:22 +0600

    Update all.js

--- scripts/commands/all.js
+++ scripts/commands/all.js
@@ -7,16 +7,20 @@ module.exports.config = {
 	commandCategory: "system",
 	usages: "[Text]",
 	cooldowns: 3
-}
 };
 
 module.exports.run = async function({ api, event, args }) {
 	try {
 		const botID = api.getCurrentUserID();
 		const listUserID = event.participantIDs.filter(ID => ID != botID && ID != event.senderID);
-		var body = (args.length != 0) ? args.join(" ") : "️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️ ️️️️️️️️️️️️️️️️️️️️️️️️️️️", mentions = [], index = 0;
+
+		var body = (args.length != 0) 
+			? args.join(" ") 
+			: "️️️️️️️️️️️️️️️️️️️️️️️️️️️";
+
+		let mentions = [], index = 0;
 		
-		for(const idUser of listUserID) {
+		for (const idUser of listUserID) {
 			body = "‎" + body;
 			mentions.push({ id: idUser, tag: "‎", fromIndex: index - 1 });
 			index -= 1;
@@ -24,6 +28,7 @@ module.exports.run = async function({ api, event, args }) {
 
 		return api.sendMessage({ body, mentions }, event.threadID, event.messageID);
 
+	} catch (e) {
+		return console.log(e);
 	}
-	catch (e) { return console.log(e); }
-}
+};
